5ec778e6a39b860fb674b42978bf82c44a940eda
Nick-GetNerdio <ntria@getnerdio.com>
2025-11-11 14:49:48 -0500

    Added section to create dynamic device group and remove need for manual input.

--- CloudShell/EnableSSOForEntraId.ps1
+++ CloudShell/EnableSSOForEntraId.ps1
@@ -1,9 +1,12 @@
+# This script enables Microsoft Entra authentication for RDP, sets the `isRemoteDesktopProtocolEnabled` property to true for the Microsoft Remote Desktop and Windows Cloud Login applications, creates a dynamic group for AVD and Windows 365 devices, and configures the target device groups.
+
 # Import the required modules
 Import-Module Microsoft.Graph.Authentication
 Import-Module Microsoft.Graph.Applications
+Import-Module Microsoft.Graph.Groups
  
 # Connect to Microsoft Graph
-Connect-MgGraph -Scopes "Application.Read.All","Application-RemoteDesktopConfig.ReadWrite.All"
+Connect-MgGraph -Scopes "Application.Read.All","Application-RemoteDesktopConfig.ReadWrite.All","Group.ReadWrite.All"
  
 # Get the object ID for each service principal
 $MSRDspId = (Get-MgServicePrincipal -Filter "AppId eq 'a4a365df-50f1-4397-bc59-1a1564b8bb9c'").Id
@@ -21,17 +24,17 @@ If ((Get-MgServicePrincipalRemoteDesktopSecurityConfiguration -ServicePrincipalI
 # Confirm the property isRemoteDesktopProtocolEnabled is set to true
 Get-MgServicePrincipalRemoteDesktopSecurityConfiguration -ServicePrincipalId $MSRDspId
 Get-MgServicePrincipalRemoteDesktopSecurityConfiguration -ServicePrincipalId $WCLspId
- 
-# Replace the placeholders with your own values
+
+# Create a dynamic device group for AVD & W365 Hosts
+$group = New-MgGroup -DisplayName "Device|AVD&W365Hosts" -Description "This is a dynamic device group for EntraID joined AVD & W365 Hosts" -MailEnabled:$false -SecurityEnabled:$true -GroupTypes "DynamicMembership" -MembershipRule '(device.devicePhysicalIds -any (_ -contains "[AzureResourceId]")) or (device.deviceModel -startsWith "Cloud PC")' -MembershipRuleProcessingState "On"
+
+# Create a targetDeviceGroup object
 $tdg = New-Object -TypeName Microsoft.Graph.PowerShell.Models.MicrosoftGraphTargetDeviceGroup
-$tdg.Id = "<Group object ID>"
-$tdg.DisplayName = "<Group display name>"
- 
+$tdg.Id = $group.Id
+$tdg.DisplayName = $group.DisplayName
+
 # Add the group to the targetDeviceGroup object
 New-MgServicePrincipalRemoteDesktopSecurityConfigurationTargetDeviceGroup -ServicePrincipalId $MSRDspId -BodyParameter $tdg
 New-MgServicePrincipalRemoteDesktopSecurityConfigurationTargetDeviceGroup -ServicePrincipalId $WCLspId -BodyParameter $tdg
  
- 
-#Please replace `<Group object ID>` and `<Group display name>` with your own values. This script enables Microsoft Entra authentication for RDP, sets the `isRemoteDesktopProtocolEnabled` property to true for the Microsoft Remote Desktop and Windows Cloud Login applications, and configures the target device groups.
- 
-#Remember to run this script in the Azure Cloud Shell or a local PowerShell session where you're signed in with Azure PowerShell and your Azure context is set to the subscription you want to use. Also, make sure you've installed the Microsoft Graph PowerShell SDK.
+# Remember to run this script in the Azure Cloud Shell or a local PowerShell session where you're signed in with Azure PowerShell and your Azure context is set to the subscription you want to use. Also, make sure you've installed the Microsoft Graph PowerShell SDK.
